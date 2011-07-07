f4497fef1a7492cb88a26ab2229e8272203a6a67
Maciej Poleski <Maciej-Poleski@users.noreply.github.com>
2011-07-07 14:59:35 +0000

    Poprawki stage2 dla windowsa
    git-svn-id: https://subversion.assembla.com/svn/DnDP.2@9 988a626b-0c67-4b66-922a-05db1d2275a1

--- win-x86/stage2.bat
+++ win-x86/stage2.bat
@@ -1,3 +1,2 @@
 @echo off
-java -cp %cd%\UpdateManager.jar org.dndp.UpdateManager.Launcher %cd% x86
-echo "a"
+java -cp "%cd%\UpdateManager.jar" org.dndp.UpdateManager.Launcher "%cd%" x86
--- win-x86_64/stage2.bat
+++ win-x86_64/stage2.bat
@@ -1,3 +1,2 @@
 @echo off
-java -cp %cd%\UpdateManager.jar org.dndp.UpdateManager.Launcher %cd% AMD64
-echo "a"
+java -cp "%cd%\UpdateManager.jar" org.dndp.UpdateManager.Launcher "%cd%" AMD64
